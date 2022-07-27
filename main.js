72fa04fdf815510aada36b4200ad6196715dd906
nquan1007 <nquan1007@gmail.com>
2022-07-27 22:20:23 +0700

    Implement TodoList

--- main.js
+++ main.js
@@ -1,76 +1,76 @@
-window.addEventListener('load', () => {
-    const form = document.querySelector('#new-task-form');
-    const input = document.querySelector('#new-task-input');
-    const listElement = document.querySelector('#tasks');
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const listElement = document.querySelector("#tasks");
 
-    form.addEventListener('submit', (e) => { // Sự việc diễn ra khi bấm Add Task
-        
-        e.preventDefault();
-        // ngăn chặn việc bấm Add Task là bị refresh
+  form.addEventListener("submit", (e) => {
+    // Sự việc diễn ra khi bấm Add Task
 
-        const task = input.value;
+    e.preventDefault();
+    // ngăn chặn việc bấm Add Task là bị refresh
 
-        if (!task) {
-            alert('Please write down your task before press Add Task');
-            return; // ? chua hieu
-        }
+    const task = input.value;
 
-        const taskElement = document.createElement("div"); // tạo thẻ div taskElement
-        taskElement.classList.add("task"); // thêm class input value vào taskElement
+    if (!task) {
+      alert("Please write down your task before press Add Task");
+      return; // ? chua hieu
+    }
 
-        const taskContentElement = document.createElement("div"); // tạo thẻ div chứa content của task
-        taskContentElement.classList.add("content"); // thêm class content vào taskContentElement
-        // taskContentElement.innerText = task;
+    const taskElement = document.createElement("div"); // tạo thẻ div taskElement
+    taskElement.classList.add("task"); // thêm class input value vào taskElement
 
-        taskElement.appendChild(taskContentElement);
+    const taskContentElement = document.createElement("div"); // tạo thẻ div chứa content của task
+    taskContentElement.classList.add("content"); // thêm class content vào taskContentElement
+    // taskContentElement.innerText = task;
 
-        const taskInputElement = document.createElement("input");
-        taskInputElement.classList.add("text");
-        taskInputElement.type = "type";
-        taskInputElement.value = task;
+    taskElement.appendChild(taskContentElement);
+
+    const taskInputElement = document.createElement("input");
+    taskInputElement.classList.add("text");
+    taskInputElement.type = "type";
+    taskInputElement.value = task;
+    taskInputElement.setAttribute("readonly", "readonly");
+
+    taskContentElement.appendChild(taskInputElement);
+
+    const taskActionsElement = document.createElement("div");
+    taskActionsElement.classList.add("actions");
+
+    const buttonEditElement = document.createElement("button");
+    buttonEditElement.classList.add("edit");
+    buttonEditElement.innerHTML = "Edit";
+
+    taskActionsElement.appendChild(buttonEditElement);
+
+    const buttonDeleteElement = document.createElement("button");
+    buttonDeleteElement.classList.add("delete");
+    buttonDeleteElement.innerHTML = "Delete";
+
+    taskActionsElement.appendChild(buttonDeleteElement);
+
+    taskElement.appendChild(taskActionsElement);
+
+    listElement.appendChild(taskElement);
+
+    input.value = ""; // sau khi add task thì input = '';
+
+    buttonEditElement.addEventListener("click", () => {
+      if (buttonEditElement.innerText.toLowerCase() == "edit") {
+        taskInputElement.removeAttribute("readonly");
+        taskInputElement.focus();
+        buttonEditElement.innerText = "save";
+      } else {
         taskInputElement.setAttribute("readonly", "readonly");
+        buttonEditElement.innerText = "edit";
+      }
+    });
 
-        taskContentElement.appendChild(taskInputElement);
-
-        const taskActionsElement = document.createElement("div");
-        taskActionsElement.classList.add("actions");
-        
-        const buttonEditElement = document.createElement("button");
-        buttonEditElement.classList.add("edit");
-        buttonEditElement.innerHTML = "Edit";
-
-        taskActionsElement.appendChild(buttonEditElement);
-
-        const buttonDeleteElement = document.createElement("button");
-        buttonDeleteElement.classList.add("delete");
-        buttonDeleteElement.innerHTML = "Delete";
-
-        taskActionsElement.appendChild(buttonDeleteElement);
-        
-        taskElement.appendChild(taskActionsElement);
-
-        listElement.appendChild(taskElement);
-
-        input.value = ""; // sau khi add task thì input = '';
-
-        buttonEditElement.addEventListener('click', () => {
-            if (buttonEditElement.innerText.toLowerCase() == "edit") {
-                taskInputElement.removeAttribute("readonly");
-                taskInputElement.focus();
-                buttonEditElement.innerText = "save";
-            } else {
-                taskInputElement.setAttribute("readonly", "readonly");
-                buttonEditElement.innerText = "edit";
-            }
-        })
-
-        buttonDeleteElement.addEventListener('click', () => {
-            listElement.removeChild(taskElement);
-        })
-    })
-})
+    buttonDeleteElement.addEventListener("click", () => {
+      listElement.removeChild(taskElement);
+    });
+  });
+});
 
 // con cac
 
 // hihi
-
